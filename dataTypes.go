8d8cace904ebff5f922619b8e0c34db948c5ac03
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-06-27 00:29:18 +0530

    Learned Data Types in GO

---
+++ dataTypes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tushar") // String Type
+	fmt.Println(234)      // Integer
+	fmt.Println(45.09)    // Float
+	fmt.Println(true)     // Boolean
+	// array and slices
+	// Maps (key-value Pair)
+}
