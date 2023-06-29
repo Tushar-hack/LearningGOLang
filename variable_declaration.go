ff23939c77fe02520b84fb03faed2b63609de02c
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-06-29 22:46:38 +0530

    Variable Declaration

---
+++ variable_declaration.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//First way of Declaring Variable
+	// var <var_name> <dataType> = <value>
+	var name string = "Tushar"
+	var surname string = "Tak"
+	var rollNumber int = 223
+	var student bool = true
+	var percentage float64 = 82.8
+	fmt.Println(name, rollNumber, student, percentage)
+}
